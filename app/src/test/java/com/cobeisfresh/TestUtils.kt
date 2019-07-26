655991ca3193b88b1577615fe9235ee6efb6eb30
Luka <luka.kordic@cobeisfresh.com>
2019-07-26 09:41:49 +0200

    Add WeatherViewModel test (failing because of the issue with live data)

---
+++ app/src/test/java/com/cobeisfresh/TestUtils.kt
@@ -0,0 +1,6 @@
+package com.cobeisfresh
+
+const val TEMP = 20.0
+const val HUMIDITY = 20
+const val PRESSURE = 1013.30
+const val OSIJEK_CITY_NAME = "Osijek"
--- app/src/test/java/com/cobeisfresh/WeatherViewModelTest.kt
+++ app/src/test/java/com/cobeisfresh/WeatherViewModelTest.kt
@@ -1,9 +1,57 @@
 package com.cobeisfresh
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.Observer
+import com.cobeisfresh.template.ui.weather.base.ViewState
+import com.cobeisfresh.template.ui.weather.presentation.WeatherViewModel
+import com.example.domain.interaction.weather.GetWeatherUseCase
+import com.example.domain.model.Success
+import com.example.domain.model.WeatherInfo
+import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.whenever
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.asCoroutineDispatcher
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.setMain
+import org.junit.After
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.TestRule
 import org.junit.runner.RunWith
 import org.mockito.junit.MockitoJUnitRunner
+import java.util.concurrent.Executors
 
 @RunWith(MockitoJUnitRunner::class)
 class WeatherViewModelTest {
-
+  
+  private val getWeather: GetWeatherUseCase = mock()
+  private val weatherViewModel by lazy { WeatherViewModel(getWeather) }
+  private val fakeMainThread = Executors.newSingleThreadExecutor().asCoroutineDispatcher()
+  private val fakeObserver: Observer<ViewState<WeatherInfo>> = mock()
+  
+  @get:Rule
+  val rule: TestRule = InstantTaskExecutorRule()
+  
+  @Before
+  fun setUp() {
+    Dispatchers.setMain(fakeMainThread)
+  }
+  
+  @Test
+  fun `test getWeather sets liveData value when success`() = runBlocking {
+    weatherViewModel.weatherLiveData.observeForever(fakeObserver)
+    whenever(getWeather(OSIJEK_CITY_NAME)).thenReturn(Success(WeatherInfo(TEMP, HUMIDITY, PRESSURE)))
+    weatherViewModel.getWeatherForLocation(OSIJEK_CITY_NAME)
+    assertEquals(ViewState.Status.LOADING, weatherViewModel.weatherLiveData.value?.status)
+  }
+  
+  @After
+  fun tearDown() {
+    Dispatchers.resetMain()
+    fakeMainThread.close()
+    weatherViewModel.weatherLiveData.removeObserver(fakeObserver)
+  }
 }
